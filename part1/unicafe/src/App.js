d01fe6d71692e55bb9e040f1260dc37a6844d21e
LittleQuartZ <58659387+LittleQuartZ@users.noreply.github.com>
2022-08-17 21:26:23 +0700

    1.8 refactor statistics component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,24 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {good + neutral + bad}</p>
+      <p>average: {(good + bad * -1) / total}</p>
+      <p>
+        positive:{' '}
+        {good && bad ? (good / total) * 100 + '%' : 'not enough feedbacks'}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -13,18 +32,7 @@ const App = () => {
         neutral
       </button>
       <button onClick={() => setBad(bad => bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {good + neutral + bad}</p>
-      <p>average: {(good + bad * -1) / (good + neutral + bad)}</p>
-      <p>
-        positive:{' '}
-        {good && bad
-          ? (good / (good + neutral + bad)) * 100 + '%'
-          : 'not enough feedbacks'}
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
